aebf1d15a237d04504b5813915350a3871d8a36b
felic <felicianurannisa4@gmail.com>
2023-10-23 12:38:27 +0700

    87% Progress

---
+++ feliTickets/Data/ViewComponents/ShoppingCartSummary.cs
@@ -0,0 +1,23 @@
+﻿using feliTickets.Data.Cart;
+using Microsoft.AspNetCore.Mvc;
+
+namespace feliTickets.Data.ViewComponents
+{
+    public class ShoppingCartSummary : ViewComponent
+    {
+        private readonly ShoppingCart _shoppingCart;
+
+        public ShoppingCartSummary(ShoppingCart shoppingCart)
+        {
+              _shoppingCart = shoppingCart;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+
+            return View(items.Count);
+        }
+    }
+    
+}
